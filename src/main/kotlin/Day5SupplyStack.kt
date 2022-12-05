2060538b30c0efc557a261676beef95003d9a60e
edward.fedorov <edward.fedorov@fintech-farm.com>
2022-12-05 18:36:48 +0100

    Day 5 Part 1

--- src/main/kotlin/Day5SupplyStack.kt
+++ src/main/kotlin/Day5SupplyStack.kt
@@ -14,6 +14,7 @@ class Day5SupplyStack {
             reversedRows[0].forEach {
                 if (it == '[') stacks.add(Stack())
             }
+
             stacks.forEachIndexed { index, chars ->
                 var row = 0
                 while (row < reversedRows.size) {
@@ -26,7 +27,50 @@ class Day5SupplyStack {
                     }
                 }
             }
-        }
 
+            stacks.forEach {
+                println(it)
+            }
+            val fileMoves = File("/Users/edwardfedorow/IdeaProjects/AdventOfCode/src/main/resources/input_day_5_2")
+            fileMoves.forEachLine {
+                var i = 0
+                var itemsToMove = 0
+                while (i < it.length) {
+                    when {
+                        it[i] == 'm' -> {
+                            // poll items
+                            i += 5
+                            var factor = 1
+                            while (it[i].isDigit()) {
+                                itemsToMove = itemsToMove * factor + it[i].digitToInt()
+                                i++
+                                factor *= 10
+                            }
+                            i++
+                        }
+
+                        it[i] == 'f' -> {
+                            i += 5
+                            val stackSender = it[i].digitToInt()
+                            i += 5
+                            val stackReceiver = it[i].digitToInt()
+                            repeat(itemsToMove) {
+                                stacks[stackReceiver - 1].push(stacks[stackSender - 1].pop())
+                            }
+                            i++
+                        }
+                    }
+                }
+            }
+
+            println("Result")
+            stacks.forEach {
+                println(it)
+            }
+            stacks.forEach {
+                print(it.peek())
+            }
+
+        }
     }
 }
