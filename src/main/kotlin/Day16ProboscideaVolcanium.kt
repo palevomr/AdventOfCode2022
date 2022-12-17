5b21a4b4b6e4eda8c37d441c3c73dfa095210c7b
edward.fedorov <edward.fedorov@fintech-farm.com>
2022-12-17 16:22:53 +0100

    Day 16 Part 2

--- src/main/kotlin/Day16ProboscideaVolcanium.kt
+++ src/main/kotlin/Day16ProboscideaVolcanium.kt
@@ -1,4 +1,5 @@
 import java.io.File
+import java.util.*
 
 class PipeSystem() {
     companion object {
@@ -14,72 +15,122 @@ class PipeSystem() {
                 val pipes = regex.findAll(it).map { it.value }.toList()
                 pipeSystem.addPipe(pipes[0], regexNumber.find(it)?.value?.toInt() ?: 0, pipes.takeLast(pipes.size - 1))
             }
+            pipeSystem.valves.values.forEach {
+                it.initPaths(pipeSystem.valves)
+            }
+            pipeSystem.valves.values.forEach {
+                it.initWithUseful(pipeSystem.useful)
+                println(it.ways)
+            }
             val time = System.currentTimeMillis()
-            println(pipeSystem.findOptimalWay())
+            println(pipeSystem.findOptimalWayWithElephant())
             val elapsed = System.currentTimeMillis() - time
             println("Elapsed time - $elapsed ms")
         }
     }
 
-    private val pipesTunnelsMap = mutableMapOf<String, List<String>>()
-    private val pipeValueMap = mutableMapOf<String, Int>()
+    private val valves = mutableMapOf<String, Valve>()
+    private val useful = mutableSetOf<Valve>()
 
     fun addPipe(pipeName: String, pipeValue: Int, tunnelsTo: List<String>) {
-        pipesTunnelsMap[pipeName] = tunnelsTo
-        pipeValueMap[pipeName] = pipeValue
-        println("Add $pipeName with $pipeValue, which leads to $tunnelsTo")
+        valves[pipeName] = Valve(pipeName, pipeValue, tunnelsTo).also {
+            if (pipeValue > 0) useful.add(it)
+        }
     }
 
-    fun findOptimalWay(): Int {
-        val opened = mutableMapOf<String, Boolean>()
+    fun findOptimalWayWithElephant(): Int {
         val memo = mutableMapOf<String, Int>()
-        fun traverse(
-            currentPipe: String,
-            leftMinutes: Int,
-            totalValue: Int,
-            currentOpenedValue: Int
-        ): Int {
-            if (memo["$currentPipe+$leftMinutes+$totalValue"] != null) {
-                return memo["$currentPipe+$leftMinutes+$totalValue"]!!
+        val shouldOpen = mutableSetOf<String>()
+        useful.forEach { shouldOpen.add(it.id) }
+        println(shouldOpen)
+        fun traverse(human: Actor, elephant: Actor, totalRelease: Int): Int {
+            val key = "$human$elephant+$totalRelease"
+            if (memo[key] != null) return memo[key]!!
+            if (human.leftMinutes <= 0 && elephant.leftMinutes <= 0) return totalRelease
+            if (shouldOpen.isEmpty()) return totalRelease
+            var max = totalRelease
+            if (elephant.leftMinutes > human.leftMinutes) {
+                val currentValve = elephant.currentValve
+                currentValve.ways.forEach { (valve, value) ->
+                    if (elephant.leftMinutes - value > 0 && shouldOpen.contains(valve.id)) {
+                        shouldOpen.remove(valve.id)
+                        val newlyReleased = (elephant.leftMinutes - value) * valve.rate
+                        val result = traverse(
+                            human,
+                            elephant.copy(currentValve = valve, leftMinutes = elephant.leftMinutes - value),
+                            totalRelease + newlyReleased
+                        )
+                        shouldOpen.add(valve.id)
+                        max = Math.max(result, max)
+                    }
+                }
+            } else {
+                human.currentValve.ways.forEach { (valve, value) ->
+                    if (human.leftMinutes - value > 0 && shouldOpen.contains(valve.id)) {
+                        shouldOpen.remove(valve.id)
+                        val newlyReleased = (human.leftMinutes - value) * valve.rate
+                        val result = traverse(
+                            human.copy(currentValve = valve, leftMinutes = human.leftMinutes - value),
+                            elephant,
+                            totalRelease + newlyReleased
+                        )
+                        shouldOpen.add(valve.id)
+                        max = Math.max(result, max)
+                    }
+                }
             }
-            if (leftMinutes <= 0) {
-                return totalValue
-            }
-            var max = 0
-            val directions = pipesTunnelsMap[currentPipe].orEmpty()
-            val currentValue = pipeValueMap[currentPipe] ?: 0
-            directions.forEach {
-                val openCurrentPipe = if (currentValue != 0
-                    && !opened.getOrDefault(currentPipe, false)
-                    && leftMinutes != 1
-                ) {
-                    opened[currentPipe] = true
-                    val result = traverse(
-                        currentPipe = it,
-                        leftMinutes = leftMinutes - 2,
-                        totalValue = totalValue + 2 * currentOpenedValue + currentValue,
-                        currentOpenedValue = currentOpenedValue + currentValue
-                    )
-                    opened[currentPipe] = false
-                    result
-                } else 0
-                val skipCurrentPipe = traverse(
-                    currentPipe = it,
-                    leftMinutes = leftMinutes - 1,
-                    totalValue = totalValue + currentOpenedValue,
-                    currentOpenedValue = currentOpenedValue
-                )
-                max = Math.max(max, Math.max(openCurrentPipe, skipCurrentPipe))
-            }
-            memo["$currentPipe+$leftMinutes+$totalValue"] = max
+            memo[key] = max
             return max
         }
-        return traverse("AA", 30, 0, 0)
+        return traverse(Actor(valves["AA"]!!, 26), Actor(valves["AA"]!!, 26), 0)
     }
 }
 
-data class Pipe(
+data class Actor(
+    var currentValve: Valve,
+    var leftMinutes: Int,
+) {
+    override fun toString(): String {
+        return "${currentValve.id}+$leftMinutes"
+    }
+}
+
+data class Valve(
+    val id: String,
     val rate: Int,
-    var opened: Boolean,
-    val leadsTo: Set<Pipe>
-)
+    val directionsRaw: List<String>
+) {
+    val directions = mutableListOf<Valve>()
+    val ways = mutableMapOf<Valve, Int>()
+
+    override fun toString(): String {
+        return "$id"
+    }
+    fun initPaths(map: Map<String, Valve>) {
+        directionsRaw.forEach {
+            directions.add(map[it]!!)
+        }
+    }
+
+    fun initWithUseful(useful: Set<Valve>) {
+        val stack = LinkedList<Valve>()
+        val distances = HashMap<Valve, Int>()
+        val visited = HashSet<Valve>()
+        stack.add(this)
+        distances[this] = 0
+        while (!stack.isEmpty() && ways.size < useful.size) {
+            val cur = stack.removeAt(0)
+            visited.add(cur)
+            val curDist = distances[cur]
+            if (cur !== this && useful.contains(cur)) {
+                ways[cur] = curDist!! + 1 // the way takes curDist minutes; +1 minute for opening the valve
+            }
+            for (valve in cur.directions) {
+                if (!visited.contains(valve)) {
+                    stack.add(valve)
+                    distances[valve] = curDist!! + 1
+                }
+            }
+        }
+    }
+}
