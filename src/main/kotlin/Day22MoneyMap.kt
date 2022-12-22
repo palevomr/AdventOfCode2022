b4c14b7e47980363563035ac48f2e973e77038ee
edward.fedorov <palevomr@gmail.com>
2022-12-22 22:44:52 +0100

    Day 22 Part 2

--- src/main/kotlin/Day22MoneyMap.kt
+++ src/main/kotlin/Day22MoneyMap.kt
@@ -1,9 +1,10 @@
 import java.io.File
 
-class MonkeyMap(val rows: Int, val cols: Int) {
-    val map = Array(rows) {
-        Array(cols) { ' ' }
-    }
+class MonkeyMap(private val rows: Int, private val cols: Int) {
+    val map = Array(rows) { Array(cols) { ' ' } }
+
+    // cubeSize
+    val size = cols / 3
 
     // stores first and last item on row
     private val rowsMap = mutableMapOf<Int, Pair<Item, Item>>()
@@ -14,7 +15,12 @@ class MonkeyMap(val rows: Int, val cols: Int) {
     //
     private var currentRow = -1
     private var currentCol = -1
-    private var rotations = arrayOf(0 to 1, 1 to 0, 0 to -1, -1 to 0)
+    private var rotations = arrayOf(
+        0 to 1, // 0
+        1 to 0, // 1
+        0 to -1, // 2
+        -1 to 0 // 3
+    )
     private var currentRotationIndex = 0
 
     fun processInput(input: List<String>) {
@@ -65,8 +71,8 @@ class MonkeyMap(val rows: Int, val cols: Int) {
     private fun move(moves: Int) {
         var current = 0
         while (current < moves) {
-            val nextRow = currentRow + rotations[currentRotationIndex].first
-            val nextCol = currentCol + rotations[currentRotationIndex].second
+            var nextRow = currentRow + rotations[currentRotationIndex].first
+            var nextCol = currentCol + rotations[currentRotationIndex].second
             if (nextRow in 0 until rows && nextCol in 0 until cols && map[nextRow][nextCol] != ' ') {
                 if (map[nextRow][nextCol] == '.') {
                     currentRow = nextRow
@@ -77,43 +83,71 @@ class MonkeyMap(val rows: Int, val cols: Int) {
                 }
             } else {
                 // wrap around
-                when (currentRotationIndex) {
-                    // right
-                    0 -> {
-                        val nextItem = rowsMap[nextRow]!!.first
-                        if (nextItem.tile != '#') {
-                            currentCol = nextItem.index
-                            currentRow = nextRow
-                            current++
-                        } else break
-                    }
-                    // bottom
-                    1 -> {
-                        val nextItem = colsMap[nextCol]!!.first
-                        if (nextItem.tile != '#') {
-                            currentRow = nextItem.index
-                            currentCol = nextCol
-                            current++
-                        } else break
-                    }
-                    // left
-                    2 -> {
-                        val nextItem = rowsMap[nextRow]!!.second
-                        if (nextItem.tile != '#') {
-                            currentCol = nextItem.index
-                            currentRow = nextRow
-                            current++
-                        } else break
-                    }
-                    // top
-                    3 -> {
-                        val nextItem = colsMap[nextCol]!!.second
-                        if (nextItem.tile != '#') {
-                            currentRow = nextItem.index
-                            currentCol = nextCol
-                            current++
-                        } else break
-                    }
+                var newRotation = -1
+                val side = sideOf(currentRow, currentCol)
+                if (side == "1" && currentRotationIndex == 3) {
+                    newRotation = 0
+                    nextCol = 0
+                    nextRow = 3 * size + currentCol - size // nextSide = F
+                } else if (side == "1" && currentRotationIndex == 2) {
+                    newRotation = 0
+                    nextCol = 0
+                    nextRow = 2 * size + (size - currentRow - 1) // nextSide = E
+                } else if (side == "2" && currentRotationIndex == 3) {
+                    newRotation = 3
+                    nextCol = currentCol - 2 * size
+                    nextRow = 4 * size - 1 // nextSide = F
+                } else if (side == "2" && currentRotationIndex == 0) {
+                    newRotation = 2
+                    nextCol = 2 * size - 1
+                    nextRow = (size - currentRow) + 2 * size - 1 // nextSide = D
+                } else if (side == "2" && currentRotationIndex == 1) {
+                    newRotation = 2
+                    nextCol = 2 * size - 1
+                    nextRow = size + (currentCol - 2 * size) // nextSide = C
+                } else if (side == "3" && currentRotationIndex == 0) {
+                    newRotation = 3
+                    nextCol = (currentRow - size) + 2 * size
+                    nextRow = size - 1
+                } else if (side == "3" && currentRotationIndex == 2) {
+                    newRotation = 1
+                    nextCol = currentRow - size
+                    nextRow = 2 * size
+                } else if (side == "4" && currentRotationIndex == 1) {
+                    newRotation = 2
+                    nextCol = size - 1
+                    nextRow = 3 * size + (currentCol - size)
+                } else if (side == "4" && currentRotationIndex == 0) {
+                    newRotation = 2
+                    nextCol = 3 * size - 1
+                    nextRow = size - (currentRow - size * 2) - 1
+                } else if (side == "5" && currentRotationIndex == 2) {
+                    newRotation = 0
+                    nextCol = size
+                    nextRow = size - (currentRow - 2 * size) - 1 // nextSide = A
+                } else if (side == "5" && currentRotationIndex == 3) {
+                    newRotation = 0
+                    nextCol = size
+                    nextRow = size + currentCol // nextSide = C
+                } else if (side == "6" && currentRotationIndex == 0) {
+                    newRotation = 3
+                    nextCol = (currentRow - 3 * size) + size
+                    nextRow = 3 * size - 1
+                } else if (side == "6" && currentRotationIndex == 2) {
+                    newRotation = 1
+                    nextCol = size + (currentRow - 3 * size)
+                    nextRow = 0 // nextSide = A
+                } else if (side == "6" && currentRotationIndex == 1) {
+                    newRotation = 1
+                    nextCol = currentCol + 2 * size
+                    nextRow = 0
+                }
+                if (map[nextRow][nextCol] == '#') break
+                else {
+                    currentRow = nextRow
+                    currentCol = nextCol
+                    currentRotationIndex = newRotation
+                    current++
                 }
             }
         }
@@ -125,6 +159,16 @@ class MonkeyMap(val rows: Int, val cols: Int) {
         } else {
             currentRotationIndex = if (currentRotationIndex == 0) 3 else (currentRotationIndex - 1)
         }
+    }
+
+    private fun sideOf(row: Int, column: Int): String {
+        if (column in size until 2 * size && row in 0 until size) return "1"
+        if (column in 2 * size until 3 * size && row in 0 until size) return "2"
+        if (column in size until 2 * size && row in size until 2 * size) return "3"
+        if (column in size until 2 * size && row in 2 * size until 3 * size) return "4"
+        if (column in 0 until size && row in 2 * size until 3 * size) return "5"
+        if (column in 0 until size && row in 3 * size until 4 * size) return "6"
+        return ""
     }
 
     companion object {
